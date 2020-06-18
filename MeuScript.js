cc376709ecb844e645c642bc5db4c81d9a134850
lucasprezom <lucasprezom@gmail.com>
2020-06-17 22:59:08 -0300

    Upando exercicio primeira vez

---
+++ MeuScript.js
@@ -0,0 +1,9 @@
+let Numero1 = ParseInt(window.prompt("Digite um numero"));
+let Numero2 = ParseInt(window.prompt("Digite um numero"));
+
+if (Numero1 > Numero2) {
+    alert("O numero " + Numero1 + "é o maior")
+} else {
+    alert("O numero " + Numero2 + "é o maior")
+}
+
